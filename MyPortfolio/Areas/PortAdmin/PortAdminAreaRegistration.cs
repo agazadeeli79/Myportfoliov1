876d2b9de1746e5d4341afb52fe118366c4302ab
Agazade <agazade7977@gmail.com>
2019-09-16 16:53:36 +0400

    first commit

---
+++ MyPortfolio/Areas/PortAdmin/PortAdminAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace MyPortfolio.Areas.PortAdmin
+{
+    public class PortAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PortAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PortAdmin_default",
+                "PortAdmin/{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new string[] { "~/Areas.PortAdmin.Controllers" }
+            );
+        }
+    }
+}
---
+++ MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+         PortfolioDB DB = new PortfolioDB();
+        public ActionResult Index()
+        {
+            ViewBag.section1 =DB.section1Divs.Find(1);
+            ViewBag.workCat = DB.CategoryWork.ToList();
+            ViewBag.workList = DB.MyWorksDiv.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
